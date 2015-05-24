3d8147c8b4f5f541bc2b532f846f95d1f6dce0c5
Fabian Schneider <fabian.schneider@lemonize.de>
2015-05-24 14:24:08 +0200

    Generated magnolia project and module.

---
+++ classroom-manager-module/src/main/java/bitcare/ClassroomManager.java
@@ -0,0 +1,12 @@
+package bitcare;
+
+/**
+ * This class is optional and represents the configuration for the classroom-manager module.
+ * By exposing simple getter/setter/adder methods, this bean can be configured via content2bean
+ * using the properties and node from <tt>config:/modules/classroom-manager</tt>.
+ * If you don't need this, simply remove the reference to this class in the module descriptor xml.
+ */
+public class ClassroomManager {
+    /* you can optionally implement info.magnolia.module.ModuleLifecycle */
+
+}
